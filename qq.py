fd6ffa79afc2477c127d30364b61750cdd45c5ec
engrbugs <engrbugs@gmail.com>
2020-12-12 16:27:01 -0800

    v0.6 2020-12-12-1626

--- qq.py
+++ qq.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-"""Launching web browsers for PowerToys Run v0.2"""
+"""Launching web browsers for PowerToys Run v0.6"""
 # Maintained by engrbugs.
 
 import re
@@ -8,7 +8,7 @@ import webbrowser
 
 
 def browse(choice, words):
-    print(f'open, {choice}, {words}')
+    print(f'opening, {choice}, {words}')
     if re.match(r'[1qazQAZ]', choice):
         webbrowser.open(f'https://www.google.com/search?q={words}')
     if re.match(r'[2wsxWSX]', choice):
@@ -18,8 +18,19 @@ def browse(choice, words):
 
 
 if __name__ == '__main__':
-    print('[1]Google,[2]Thesaurus,[3]Youtube', end=">")
+    print('*Default')  # New Line
+    print('[1*]Google,[2]Thesaurus,[3]Youtube', end=">    ")
     inputted_string = input()
+    #  Clean white spaces from beginning to end.
     x = inputted_string.strip()
-    if re.match(r'^[1-3]|[qweQWE]|[asdASD]|[zxcZXC]', x):
+    #  When inputted with ONE CHAR, it will ask for what to browse.
+    if re.match(r'(?i)^[1-3]$|^[qwe]$|^[asd]$|^[zxc]$', x):
+        print('What to search', end=":                        ")
+        word = input()
+        browse(x, word.strip())
+    #  Automatically extract the first char when detects whitespace on the 2nd char
+    elif re.match(r'(?i)^([1-3]|[qwe]|[asd]|[zxc])\s', x):
         browse(x[0:1], x[1:len(x)].strip())
+    #  Search with default option--Google.
+    else:
+        browse('1', x)
