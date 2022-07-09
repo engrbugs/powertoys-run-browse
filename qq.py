115a80ab7837ff7b213746ab1dc5261f8290a62d
engrbugs <engrbugs@gmail.com>
2022-07-09 00:03:45 -0700

    2022-07-09-0003

--- qq.py
+++ qq.py
@@ -3,60 +3,50 @@ import re
 import webbrowser
 
 """Launching web browsers for PowerToys Run"""
-version = '1.0.5'
+version = '1.0.6'
 default_options = '1'
 
 SHORTCUTS = {
     "Define": ['https://www.google.com/search?q={words} define', '1', 'q'],
-    "Thesaurus": ['https://www.google.com/search?q={words} define', '2', 't'],
-    "Youtube": ['https://www.google.com/search?q={words} define', '3', 'y'],
-    "Reverse Dictionary": ['https://www.google.com/search?q={words} define', '4', 'q'],
-    "Google Pronunciation": ['https://www.google.com/search?q={words} define', '5', 'p'],
-    "Github": ['https://www.google.com/search?q={words} define', '6', 'g'],
-    "stackoverflow": ['https://www.google.com/search?q={words} define', '7', 's'],
+    "Thesaurus": ['https://www.google.com/search?q={words} thesaurus', '2', 'w'],
+    "Youtube": ['https://www.youtube.com/results?search_query={words}', '3', 'y'],
+    "Reverse Dictionary": ['https://www.onelook.com/reverse-dictionary.shtml?s={words}', '4', 'q'],
+    "Google Pronunciation": ['https://www.google.com/search?q={words} pronunciation', '5', 'p'],
+    "Github": ['https://github.com/search?q={words}', '6', 'g'],
+    "stackoverflow": ['https://stackoverflow.com/search?q={words}', '7', 's'],
     "Calendar": ['https://www.google.com/search?q={words} define', '8', 'c'],
-    "Reddit": ['https://www.google.com/search?q={words} define', 'R'],
-    "Twitter": ['https://www.google.com/search?q={words} define', 'T'],
+    "Reddit": ['https://www.reddit.com/', 'R'],
+    "Twitter": ['https://www.twitter.com/', 'T'],
 }
 # Maintained by engrbugs.
 
 
 def browse(browse_choice, words):
     print(f'opening, {browse_choice}, {words}')
-    if re.match(r'[1qQ]', browse_choice):
-        # check '#' character in choice replace it with %23 if found.
-        google_word = ''
-        for c in words:
-            google_word += '%23' if c == '#' else c
-        webbrowser.open(f'https://www.google.com/search?q={google_word} define')
-    elif re.match(r'[2]', browse_choice):
-        webbrowser.open(f'https://www.google.com/search?q={words} thesaurus')
-    elif re.match(r'[3yY]', browse_choice):
-        if words == 'main':
-            webbrowser.open(f'https://www.youtube.com')
-        else:
-            webbrowser.open(f'https://www.youtube.com/results?search_query={words}')
-    elif re.match(r'[4]', browse_choice):
-        webbrowser.open(f'https://www.onelook.com/reverse-dictionary.shtml?s={words}')
-    elif re.match(r'[5pP]', browse_choice):
-        webbrowser.open(f'https://www.google.com/search?q={words} pronunciation')
-    elif re.match(r'[rR]', browse_choice):
-        webbrowser.open(f'https://www.reddit.com/')
-    elif re.match(r'[6]', browse_choice):
-        if words == 'main':
-            webbrowser.open(f'https://github.com')
-        else:
-            webbrowser.open(f'https://github.com/search?q={words}')
-    elif re.match(r'[7]', browse_choice):
-        if words == 'main':
-            webbrowser.open(f'https://stackoverflow.com')
-        else:
-            webbrowser.open(f'https://stackoverflow.com/search?q={words}')
+    for k in SHORTCUTS:
+        if any(a in SHORTCUTS[k] for a in [browse_choice.lower(), browse_choice.upper()]):
+            if '{' in SHORTCUTS[k][0]:
+                # check '#' character in choice replace it with %23 if found. for C#
+                if '#' in words:
+                    google_word = ''
+                    for c in words:
+                        google_word += '%23' if c == '#' else c
+                    words = google_word
+                if words == '':
+                    print('What to search', end=":                        ")
+                    words = input()
+                if words == 'main':
+                    main_website = SHORTCUTS[k][0][:SHORTCUTS[k][0].find('/', 9)+1]
+                    webbrowser.open(main_website)
+                    quit()
+                webbrowser.open(SHORTCUTS[k][0].format(words=words))
+            else:
+                webbrowser.open(SHORTCUTS[k][0])
 
 
 if __name__ == '__main__':
 
-    #   PAINT SCREEN.
+    #   PAINT SCREEN
     is_default = lambda x: x + "*" if x == default_options else x
     print(f'*Default v{version}')  # New Line
     keys = list(SHORTCUTS)
@@ -69,21 +59,19 @@ if __name__ == '__main__':
         elif i % 2 == 1:
             print(s)
 
+    #   INPUT MODE
     inputted_string = input()
-    #  Clean white spaces from beginning to end.
     inputted_string = inputted_string.strip()
-    #  When inputted with ONE CHAR, it will ask for what to browse.
-    if re.match(r'(?i)^[1-7]$|^[qwep]$|^[asdf]$|^[gty]$|^[r]$', inputted_string):
-        if inputted_string.lower() == 'r':
-            browse(inputted_string, '')
-            quit()
-        print('What to search', end=":                        ")
-        word = input()
-        browse(inputted_string, word.strip())
-    #  Automatically extract the first char when detects whitespace on the 2nd char
-    elif re.match(r'(?i)^([1-7]|[qwep]|[asdf]|[gty])\s', inputted_string):
-        browse(inputted_string[0:1], inputted_string[1:len(inputted_string)].strip())
 
+    # OLD REGEX : re.match(r'(?i)^[1-7]$|^[qwep]$|^[asdf]$|^[gty]$|^[r]$', inputted_string)
+    # Put all shortcuts in one string
+    regex = ''
+    for i in SHORTCUTS.items():
+        for ii in i[1][1:]:
+            regex += ii
+
+    if re.match(fr'(?i)^([{regex}])|\s', inputted_string):
+        browse(inputted_string[0:1], inputted_string[1:len(inputted_string)].strip())
     elif inputted_string.lower() == 'exit':
         quit()
     else:
